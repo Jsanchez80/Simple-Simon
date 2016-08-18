6118a62d121bf8767ac8bf627254f5e9d8720042
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-08-18 15:00:38 -0500

    ***Update: JS for simple_simon project: JS currently has a random number generator and switch statement to give a visual output in the console.log...this operation will  generate the first, second, third and fourth sequence numbers while providing the string "I'm red" etc.

---
+++ public/js/simple_simon.js
@@ -0,0 +1,86 @@
+// This sequence will produce a random number between 1 and 4/////////////////////////
+var firstSequence = Math.floor(Math.random() * 4) + 1
+console.log(firstSequence)
+
+
+// switch (secondSequence)
+// The switch statement allows us to take the value of the random number represented by firstSequence
+// and add produce a value color as defined by the case #. The case number correlates with the
+// designated button value in html.
+switch (firstSequence) {
+	case 1:
+		console.log("I'm red.");
+		break;
+	case 2:
+		console.log("I'm green.");
+		break;
+	case 3:
+		console.log("I'm blue.");
+		break;
+	case 4:
+		console.log("I'm yellow");
+		break;
+
+} 
+
+var secondSequence = Math.floor(Math.random() * 4) + 1 
+console.log(secondSequence)
+
+switch (secondSequence) {
+	case 1:
+		console.log("I'm red.");
+		break;
+	case 2:
+		console.log("I'm green.");
+		break;
+	case 3:
+		console.log("I'm blue.");
+		break;
+	case 4:
+		console.log("I'm yellow");
+		break;
+
+} 
+
+var thirdSequence = Math.floor(Math.random() * 4) + 1 
+console.log(thirdSequence)
+
+switch (thirdSequence) {
+	case 1:
+		console.log("I'm red.");
+		break;
+	case 2:
+		console.log("I'm green.");
+		break;
+	case 3:
+		console.log("I'm blue.");
+		break;
+	case 4:
+		console.log("I'm yellow");
+		break;
+
+} 
+
+var fourthSequence = Math.floor(Math.random() * 4) + 1 
+console.log(thirdSequence)
+
+switch (fourthSequence) {
+	case 1:
+		console.log("I'm red.");
+		break;
+	case 2:
+		console.log("I'm green.");
+		break;
+	case 3:
+		console.log("I'm blue.");
+		break;
+	case 4:
+		console.log("I'm yellow");
+		break;
+
+} 
+
+
+
+
+
